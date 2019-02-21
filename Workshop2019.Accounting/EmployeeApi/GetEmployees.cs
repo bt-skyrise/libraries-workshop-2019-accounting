762718f5e0782be4bc5ad0a4188440cf80293e9e
Ryszard Tarajkowski <ryszard.tarajkowski@btsignaal.com>
2019-02-21 15:38:37 +0100

    Implemented Accounting app.

---
+++ Workshop2019.Accounting/EmployeeApi/GetEmployees.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+namespace Workshop2019.Accounting.EmployeeApi
+{
+    public class GetEmployees
+    {
+        private readonly EmployeeApiHttpClient _httpClient;
+
+        public GetEmployees(EmployeeApiHttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<GetEmployeeDto[]> Get()
+        {
+            var response = await _httpClient.Get<GetEmployeesResponse>("api/employees");
+
+            return response.Employees;
+        }
+    }
+
+    public class GetEmployeesResponse
+    {
+        public GetEmployeeDto[] Employees { get; set; }
+    }
+
+    public class GetEmployeeDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+    }
+}
--- Workshop2019.Accounting/Program.cs
+++ Workshop2019.Accounting/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Workshop2019.Accounting.EmployeeApi;
 
 namespace Workshop2019.Accounting
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            
+            var employeeApi = new EmployeeApiHttpClient(
+                apiUrl: new Uri(configuration["EmployeeApiUrl"]),
+                apiKey: configuration["EmployeeApiKey"]
+            );
+
+            var getEmployees = new GetEmployees(employeeApi);
+            var getEmployeeWorkLog = new GetEmployeeWorkLog(employeeApi);
+            var getEmployeeOvertime = new GetEmployeeOvertime(getEmployeeWorkLog);
+
+            Console.WriteLine("Accounting overtime report:");
+
+            foreach (var employee in await getEmployees.Get())
+            {
+                var employeeOvertime = await getEmployeeOvertime.Get(
+                    employeeId: employee.Id,
+                    from: new DateTime(2019, 2, 1),
+                    to: new DateTime(2019, 2, 28)
+                );
+
+                Console.WriteLine($"    {employee.FirstName} {employee.Surname}, total overtime: {employeeOvertime}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
