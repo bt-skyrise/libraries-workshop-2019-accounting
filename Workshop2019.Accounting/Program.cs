b9faba33ebfb7ae1e6c4d489b7082dfab72c7502
Ryszard Tarajkowski <ryszard.tarajkowski@btsignaal.com>
2019-02-21 15:34:19 +0100

    Add project files.

---
+++ Workshop2019.Accounting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Workshop2019.Accounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
